b45b7aceb831f2dc2282d82e08360c65785d9fb9
JeppeSRC <jeppe97rc@gmail.com>
2016-08-25 15:10:54 +0200

    Refactored everything to use definitions in stddef

--- Kernel/stdlib/include/memory.h
+++ Kernel/stdlib/include/memory.h
@@ -1,7 +1,8 @@
 #pragma once
+#include "stddef.h"
 
 #define memset(dst, v, s) _memset(dst, v, s)
 #define memcpy(dst, src, s) _memcpy(dst, src, s)
 
-extern void _memset(void* _dst, unsigned char _value, unsigned int _size);
-extern void _memcpy(void* _dst, const void* const _src, unsigned int _size);
+extern void _memset(void* _dst, unsigned char _value, uint32 _size);
+extern void _memcpy(void* _dst, const void* const _src, uint32 _size);
